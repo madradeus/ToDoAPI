88138464dcd67b77b4e61fbd637a7306a1bcca60
Mateusz Rodowski <mateusz.rodowski@gmail.com>
2022-11-20 22:56:48 +0100

    delete asyncWrapper.ts file

--- utils/asyncWrapper.ts
+++
@@ -1,9 +0,0 @@
-import {NextFunction, Request, Response} from "express";
-
-export function runAsyncWrapper (callback: any) {
-    return function (req: Request, res: Response, next: NextFunction) {
-        callback(req, res, next)
-            .catch(next)
-    }
-}
-
